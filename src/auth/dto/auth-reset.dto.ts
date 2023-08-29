fa67739422436250e03206a67de6d8d0b90f3864
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2023-08-29 08:17:16 -0300

    feat: Set up NestJS with Prisma and MySQL

---
+++ src/auth/dto/auth-reset.dto.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import {IsJWT, IsStrongPassword } from 'class-validator';
+
+export class AuthResetDTO {
+  @IsStrongPassword({
+    minLength: 6,
+    minLowercase: 0,
+    minUppercase: 0,
+    minNumbers: 0,
+    minSymbols: 0,
+  })
+  password: string;
+
+  @IsJWT()
+  token: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Put,
+  Patch,
+  Delete,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
+import { ParamId } from 'src/decorators/param-id.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { RoleGuard } from 'src/guards/role.guard';
+import { AuthGuard } from 'src/guards/auth.guard';
+
+// @UseInterceptors(LogInterceptor) para usar no controller todo
+@Roles(Role.Admin)
+@UseGuards(AuthGuard, RoleGuard)
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // @UseInterceptors(LogInterceptor) para usar só em uma função
+  @Post()
+  async create(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
+  }
+
+  @Roles(Role.Admin, Role.User)
+  @Get()
+  async read() {
+    return this.userService.read();
+  }
+
+  @Get(':id')
+  async readOne(@ParamId() id: number) {
+    return this.userService.readOne(id);
+  }
+
+  @Put(':id')
+  async update(@Body() data: UpdatePutUserDTO, @ParamId() id) {
+    return this.userService.update(id, data);
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id) {
+    return this.userService.updatePartial(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id) {
+    return this.userService.delete(id);
+  }
+}
